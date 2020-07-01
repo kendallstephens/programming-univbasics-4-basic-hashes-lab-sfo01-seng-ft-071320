c6e2df1d4c3f500ab4d57ba705fe6c06e7bb36b7
Kendall Stephens <kendoll.stephens@gmail.com>
2020-07-01 20:28:16 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -3,7 +3,7 @@ def new_hash
 end
 
 def my_hash
-  my_hash = {artist: ""
+  my_hash = {artist: "David Bowie" album: "The Rise and Fall of Ziggy Stardust and the Spiders from Mars"
 end
 
 def pioneer
